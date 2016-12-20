d3b24c8e5779d69cf7e1199010d3eb0bb9b4810c
lloydtorres <lloyd.torres95@gmail.com>
2016-12-20 00:56:35 -0500

    Change Heroku binding call.

--- server.js
+++ server.js
@@ -31,9 +31,12 @@ function main() {
   server.addProtoService(proto.UserManager.service, {getBestPersonality: getBestPersonality});
   let url = "localhost:8080";
   if (process.env.PORT) {
-    url = "bmo-wkrpt401-grpc.herokuapp.com:" + process.env.PORT
+    url = "https://bmo-wkrpt401-grpc.herokuapp.com:" + process.env.PORT
+    server.bind(url);
+  } else {
+    server.bind(url, grpc.ServerCredentials.createInsecure());
   }
-  server.bind(url, grpc.ServerCredentials.createInsecure());
+
   server.start();
   console.log("WKRPT401 gRPC server starting @ " + url);
 }
